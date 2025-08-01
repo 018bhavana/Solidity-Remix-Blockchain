48b02e80104b14a6db17229da32b70261589f332
Buddareddy Gari Bhavana <bhavanabuddareddygari07@gmail.com>
2025-08-01 20:16:48 +0530

    Add files via upload

---
+++ Base.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Base {
+    address public owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner {
+        require(msg.sender == owner, "Not owner");
+        _;
+    }
+}
+
+// Derived.sol
+pragma solidity ^0.8.0;
+import "./Base.sol";
+
+contract Derived is Base {
+    string public data;
+
+    function setData(string memory _data) public onlyOwner {
+        data = _data;
+    }
+}
---
+++ Fallback.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract FunctionTypes {
+    uint public x = 100;
+
+    function getX() public view returns (uint) {
+        return x;
+    }
+
+    function calculate(uint a, uint b) public pure returns (uint) {
+        return a + b;
+    }
+
+    receive() external payable {}
+
+    fallback() external payable {}
+}
